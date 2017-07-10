a511371773363d1021a21328954bc4abfc2b4554
sandip-markelytics <sandip@markelytics.com>
2017-07-10 22:00:08 +0530

    Update Order by

--- app/Libraries/Repositories/ProductRepository.php
+++ app/Libraries/Repositories/ProductRepository.php
@@ -46,7 +46,7 @@ class ProductRepository
             }
         };
 
-        return [$query, $attributes];
+        return [$query->orderBy('created_at', 'desc'), $attributes];
 
     }
 	public function fullTextSearch($input)
